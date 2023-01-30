28d34221b3e6ccdf09db237e39ac7a8c12f6ec10
Sofiane <98749448+kebciSofiane@users.noreply.github.com>
2023-01-30 17:19:20 +0100

    breadth_first_search

--- Taquin/src/Breadth_First_Search.java
+++ Taquin/src/Breadth_First_Search.java
@@ -3,13 +3,13 @@ import java.util.LinkedList;
 
 public class Breadth_First_Search {
 
+    LinkedList<char[][]> open  = new LinkedList<>();
+    LinkedList<char[][]> close = new LinkedList<>();
     private char[][] initialGrid;
     private char[][] finalGrid;
 
     private  int row;
-
     private  int col;
-
 
 
 
@@ -22,85 +22,83 @@ public class Breadth_First_Search {
 
 
     public void run_Breadth_Search(){
-        LinkedList<char[][]> open  = new LinkedList<>();
-        LinkedList<char[][]> close = new LinkedList<>();
-        open.add(initialGrid);
+
+        open.addLast(this.initialGrid);
         int emptyCaseX = -1;
         int emptyCaseY = -1;
 
 
         while (! open.isEmpty()){
+
             char[][] myGrid = open.poll();
-            if (myGrid == finalGrid)
+
+            if (equals(myGrid,finalGrid)){
+                System.out.println("Final grid found with breadth_first_search: "+Arrays.deepToString(myGrid));
+                System.out.println("Number of treated states: "+ close.toArray().length+ " states" );
+                System.out.println("Number of waiting states: "+ open.toArray().length+ " states" );
+
+
                 break;
+            }
 
             for (int i= 0; i< row; i++)
-                for (int j= 0; j< col;j++){
+                for (int j= 0; j< col;j++)
                     if (myGrid[i][j]==' '){
                         emptyCaseX =i;
                         emptyCaseY =j;
                     }
-                }
+
+
             System.out.println(emptyCaseX+" "+emptyCaseY);
 
 
-            char[][] transitionalGrid;
-
-            if (canBeAccessed(emptyCaseX,emptyCaseY-1))
-            {
-                System.out.println(Arrays.deepToString(myGrid));
-                transitionalGrid=myGrid;
-                transitionalGrid[emptyCaseX][emptyCaseY] = transitionalGrid[emptyCaseX][emptyCaseY-1];
-                transitionalGrid[emptyCaseX][emptyCaseY-1] = ' ';
-                System.out.println(Arrays.deepToString(transitionalGrid));
-
-                if (!close.contains(transitionalGrid) & !open.contains(transitionalGrid) )
-                    open.add(transitionalGrid);
-
-            }
-            if (canBeAccessed(emptyCaseX-1,emptyCaseY))
-            {
-                transitionalGrid=myGrid;
-                System.out.println(Arrays.deepToString(transitionalGrid));
-
-                transitionalGrid[emptyCaseX][emptyCaseY] = transitionalGrid[emptyCaseX-1][emptyCaseY];
-                transitionalGrid[emptyCaseX-1][emptyCaseY] = ' ';
-                if (!close.contains(transitionalGrid) & !open.contains(transitionalGrid) )
-                    open.add(transitionalGrid);
-                System.out.println(Arrays.deepToString(transitionalGrid));
-
-            }
-            if (canBeAccessed(emptyCaseX+1,emptyCaseY))
-            {
-                transitionalGrid=myGrid;
-                transitionalGrid[emptyCaseX][emptyCaseY] = transitionalGrid[emptyCaseX+1][emptyCaseY];
-                transitionalGrid[emptyCaseX+1][emptyCaseY] = ' ';
-                if (!close.contains(transitionalGrid) & !open.contains(transitionalGrid) )
-                    open.add(transitionalGrid);
-
-            }
-            if (canBeAccessed(emptyCaseX,emptyCaseY+1))
-            {
-                transitionalGrid=myGrid;
-                transitionalGrid[emptyCaseX][emptyCaseY] = transitionalGrid[emptyCaseX][emptyCaseY+1];
-                transitionalGrid[emptyCaseX][emptyCaseY+1] = ' ';
-
-                if (!close.contains(transitionalGrid) & !open.contains(transitionalGrid) )
-                    open.add(transitionalGrid);
-            }
+            neighborCell(open, close, emptyCaseX, emptyCaseY-1, myGrid,emptyCaseX,emptyCaseY);
+            neighborCell(open, close, emptyCaseX-1, emptyCaseY, myGrid,emptyCaseX,emptyCaseY);
+            neighborCell(open, close, emptyCaseX, emptyCaseY+1, myGrid,emptyCaseX,emptyCaseY);
+            neighborCell(open, close, emptyCaseX+1, emptyCaseY, myGrid,emptyCaseX,emptyCaseY);
 
             close.add(myGrid);
-
+        }
+    }
+    private void neighborCell(LinkedList<char[][]> open, LinkedList<char[][]> close, int x, int y, char[][] grid,int xb,int yb) {
+        char[][] transitionalGrid = deepCopy(grid);
+        if (canBeAccessed(x, y))
+        {
+            transitionalGrid[xb][yb] = grid[x][y];
+            transitionalGrid[x][y] = ' ';
+            System.out.println(Arrays.deepToString(transitionalGrid));
+            if (!isIn(close, transitionalGrid) & !isIn(open,transitionalGrid) )
+                open.addLast(transitionalGrid);
 
         }
-
     }
 
+    private boolean isIn(LinkedList<char[][]> llist, char[][] grid){
+        for(int i = 0 ; i < llist.size(); i++){
+            if (equals(llist.get(i),grid)) return true;}
+        return false;
+    }
 
+    private  char[][] deepCopy(char[][] grid){
+        char[][] newGrid = new char[row][col];
+        for (int i=0; i<row; i++)
+            for (int j=0; j<row; j++)
+                newGrid[i][j] = grid[i][j];
+        return newGrid;
+    }
     private boolean canBeAccessed(int i, int j){
         if (i<row & i>-1 & j<col & j>-1)
             return true;
         else
             return false;
     }
+
+    public boolean equals(char[][] o, char[][] o2){
+            for (int i=0;i<row;i++)
+                for (int j=0;j<col;j++)
+                    if (o[i][j] != o2[i][j])
+                        return false;
+        return true;
+    }
+
 }
--- Taquin/src/Main.java
+++ Taquin/src/Main.java
@@ -1,22 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
-        char[][] grid = new char[2][2];
-        char[][] fGrid = new char[2][2];
+        char[][] grid = new char[3][3];
+        char[][] fGrid = new char[3][3];
 
         grid[0][0] = '1';
-        grid[0][1] = '3';
-        grid[1][0] = '2';
-        grid[1][1] = ' ';
+        grid[0][1] = '2';
+        grid[0][2] = '3';
+        grid[1][0] = '7';
+        grid[1][1] = '4';
+        grid[1][2] = '5';
+        grid[2][0] = '8';
+        grid[2][1] = ' ';
+        grid[2][2] = '6';
+
+
 
         fGrid[0][0] = '1';
         fGrid[0][1] = '2';
-        fGrid[1][0] = '3';
-        fGrid[1][1] = ' ';
+        fGrid[0][2] = '3';
+        fGrid[1][0] = '4';
+        fGrid[1][1] = '5';
+        fGrid[1][2] = '6';
+        fGrid[2][0] = '7';
+        fGrid[2][1] = '8';
+        fGrid[2][2] = ' ';
+        Breadth_First_Search breadth_first_search = new Breadth_First_Search(grid,fGrid);
 
-        Breadth_First_Search depth_first_search = new Breadth_First_Search(grid,fGrid);
-
-        depth_first_search.run_Breadth_Search();
+        breadth_first_search.run_Breadth_Search();
 
 
 
