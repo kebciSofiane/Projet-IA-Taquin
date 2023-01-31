bb947fd5905385f1b6df6db1a575e020d8667a02
Sofiane <98749448+kebciSofiane@users.noreply.github.com>
2023-02-01 00:25:15 +0100

    read the files breadth_first_search working

---
+++ Taquin/src/FileReader.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileReader {
+
+
+    public ArrayList<char[][]> readFileData(String fileName){
+        ArrayList<char[][]> meyGrids = new ArrayList<>();
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+
+            int line =1;
+
+            int fillRawFinalGrid=0;
+            int fillRawInitialGrid=1;
+
+            int numberOfraws = Integer.parseInt(myReader.nextLine());
+            int numberOfcols =0;
+
+            String firstRaw="";
+
+            if (myReader.hasNextLine()) {
+                firstRaw = myReader.nextLine();
+                numberOfcols=firstRaw.length();
+            }
+            char[][] initialGrid = new char[numberOfraws][numberOfcols];
+            char[][] finalGrid = new char[numberOfraws][numberOfcols];
+
+            for (int i=0; i<firstRaw.length(); i++)
+                initialGrid[0][i] =  firstRaw.charAt(i);
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
+                if (data.length() !=0)
+                    if ( line<numberOfraws){
+                        for (int i=0; i<data.length(); i++)
+                            initialGrid[fillRawInitialGrid][i] =  data.charAt(i);
+                        fillRawInitialGrid++;}
+
+                    else {
+                        for (int i=0; i<data.length(); i++)
+                            finalGrid[fillRawFinalGrid][i] =  data.charAt(i);
+                        fillRawFinalGrid++;
+                    }
+                line++;
+            }
+            myReader.close();
+            meyGrids.add(initialGrid);
+            meyGrids.add(finalGrid);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return meyGrids;
+    }
+
+}
--- Taquin/src/Main.java
+++ Taquin/src/Main.java
@@ -1,33 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        char[][] grid = new char[3][3];
-        char[][] fGrid = new char[3][3];
+        FileReader fileReader = new FileReader();
+        ArrayList<char[][]> myGrids= fileReader.readFileData
+                ("Taquin/problems/taquin_3x3c.grid.txt");
 
-        grid[0][0] = '1';
-        grid[0][1] = '2';
-        grid[0][2] = '3';
-        grid[1][0] = '7';
-        grid[1][1] = '4';
-        grid[1][2] = '5';
-        grid[2][0] = '8';
-        grid[2][1] = ' ';
-        grid[2][2] = '6';
-
-
-
-        fGrid[0][0] = '1';
-        fGrid[0][1] = '2';
-        fGrid[0][2] = '3';
-        fGrid[1][0] = '4';
-        fGrid[1][1] = '5';
-        fGrid[1][2] = '6';
-        fGrid[2][0] = '7';
-        fGrid[2][1] = '8';
-        fGrid[2][2] = ' ';
+        char[][] grid = myGrids.get(0);
+        char[][] fGrid = myGrids.get(1);
 
         Breadth_First_Search breadth_first_search = new Breadth_First_Search(grid,fGrid);
         breadth_first_search.run_Breadth_Search();
+
 
 
 
